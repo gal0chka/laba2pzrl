3ab7ce3c2e4bb19d056866c075e8bdf652f79e97
Artemii <artemiigalo@mail.ru>
2023-12-19 21:01:14 +0300

    make func bin

---
+++ bin.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "bin.h"
+
+int bintodec(char* str)
+{
+	int ans = 0;
+	int i = 0;
+	if (str[0] = '-')
+	{
+		i++;
+	}
+	for (i; i<strlen(str) - 1; i++)
+	{
+		if (str[i] = '1')
+		{
+			ans = ans + pow(2, strlen(str) - i - 2);
+		}
+		if (str[i]!='0' && str[i]!='1')
+		{
+			printf("WRONG TYPE\n");
+			exit(-1);
+		}
+	}
+	if (str[0] = '-')
+	{
+		return -ans;
+	}
+	else
+	{
+		return ans;
+	}
+}
+
+char* dectobin(int n)
+{
+	int ramainder;
+	char* binary = (char*)malloc(100*sizof(char));
+	int i = 0;
+	while (n!=0)
+	{
+		remainder = n % 2;
+		n = n / 2;
+		binary[i++] = remainder + '0';
+	}
+	binary[i] = '\0';
+
+	char* result = (char*)malloc((i+1)*sizeof(char));
+	int k = 0;
+	for (int j = i - 1; j >= 0; j--)
+	{
+		result[k++] = binary[j];
+	}
+	result[k] = '\0';
+	free(binary);
+	return result;
+}
+
+
---
+++ bin.h
@@ -0,0 +1,5 @@
+#ifndef BIN_H
+#define BIN_H
+int bintodec(char* str);
+char* dectobin(int n);
+#endif
